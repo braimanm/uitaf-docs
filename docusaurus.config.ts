b0a78fee27b681d7986b91482cc17d36a6493543
Michael Braiman <braimanm@gmail.com>
2024-08-28 15:29:53 -0400

    Remove edit link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,8 +38,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/braimanm/uitaf-docs',
+          //editUrl: 'https://github.com/braimanm/uitaf-docs',
         },
         blog: false,
         theme: {
